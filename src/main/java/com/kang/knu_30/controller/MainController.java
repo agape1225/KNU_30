e517b812e7db99a06a46093248553722331daae8
agape1225 <49804691+agape1225@users.noreply.github.com>
2021-11-21 18:25:18 +0900

    Update MainController.java

--- src/main/java/com/kang/knu_30/controller/MainController.java
+++ src/main/java/com/kang/knu_30/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.kang.knu_30.controller;
 
+import com.kang.knu_30.service.CelebrateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,9 +12,15 @@ import java.util.List;
 @Controller
 public class MainController {
 
+    @Autowired
+    CelebrateService celebrateService;
+
     @RequestMapping("/")
     public ModelAndView main() throws Exception{
         ModelAndView mav = new ModelAndView("index");
+        mav.addObject("comment_list", celebrateService.get_celebrate_with_num(9));
+
+
 
         return mav;
     }
